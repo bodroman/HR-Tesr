e793d983cc4564ad63c533c00558a579039a3c2f
bodroman <roman.galay7@gmail.com>
2024-04-18 18:53:21 +0300

    hgjunb

--- one.py
+++ one.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+
+#  in = "userInBlocked"
+#  out = "user_in_blocked"
+
+def return_pascal_in_snake(name: str):
+    res = ""
+    for char in name:
+        if char.isupper():
+            res += "_" + char.lower()
+        else:
+            res += char
+
+s = "userInBlocked"
+
+snaked_s = return_pascal_in_snake(s)
+print(snaked_s)
